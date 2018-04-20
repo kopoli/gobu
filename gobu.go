a98f7ad3f8cbc90159ee7b865b685d8c953e3383
Kalle Kankare <kalle.kankare@iki.fi>
2018-04-20 23:14:01 +0300

    gobu: initial commit

---
+++ gobu.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	util "github.com/kopoli/go-util"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+var (
+	majorVersion     = "0"
+	version          = "Undefined"
+	timestamp        = "Undefined"
+	progVersion      = majorVersion + "-" + version
+	exitValue    int = 0
+)
+
+func fault(err error, message string, arg ...string) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s%s: %s\n", message, strings.Join(arg, " "), err)
+		exitValue = 1
+		os.Exit(exitValue)
+	}
+}
+
+func main() {
+	opts := util.NewOptions()
+
+	opts.Set("program-name", os.Args[0])
+	opts.Set("program-version", progVersion)
+	opts.Set("program-timestamp", timestamp)
+
+	rep, err := git.PlainOpen(".")
+	fault(err, "repo open")
+
+	wt, err := rep.Worktree()
+	fault(err, "worktree open")
+
+	st, err := wt.Status()
+	fault(err, "worktree status")
+	fmt.Println(st.String())
+}
