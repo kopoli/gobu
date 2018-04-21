1502134bd8ebd93af04974647a9e13e328fdf6dd
Kalle Kankare <kalle.kankare@iki.fi>
2018-04-21 21:53:00 +0300

    gobu: semi working version

--- gobu.go
+++ gobu.go
@@ -1,43 +1,134 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
+	"sort"
 	"strings"
+	"time"
 
 	util "github.com/kopoli/go-util"
-	"gopkg.in/src-d/go-git.v4"
 )
 
 var (
-	majorVersion     = "0"
-	version          = "Undefined"
-	timestamp        = "Undefined"
-	progVersion      = majorVersion + "-" + version
-	exitValue    int = 0
+	majorVersion       = "0"
+	version            = "Undefined"
+	timestamp          = "Undefined"
+	buildGOOS          = "Undefined"
+	buildGOARCH        = "Undefined"
+	progVersion        = majorVersion + "-" + version
+	exitValue    int   = 0
+	gb           *GoBu = &GoBu{}
 )
 
 type GoBu struct {
-	ldflags     string
-	buildflags  []string
-	environment map[string]string
-	subcmd      string
+	ldflags    []string
+	buildflags []string
+	environ    []string
+	subcmd     string
 }
 
 func (g *GoBu) AddLdFlags(flags ...string) {
+	g.ldflags = append(g.ldflags, flags...)
+}
 
+func (g *GoBu) AddVar(name, value string) {
+	g.AddLdFlags("-X", fmt.Sprintf("%s=%s", name, value))
 }
 
 func (g *GoBu) AddBuildFlags(flags ...string) {
-
+	g.buildflags = append(g.buildflags, flags...)
 }
 
 func (g *GoBu) SetEnv(key, value string) {
-
+	g.environ = append(g.environ, fmt.Sprintf("%s=%s", key, value))
 }
 
-func (g *GoBu) Getcmd() (command []string, env map[string]string) {
-	return nil, nil
+func (g *GoBu) Getcmd() (command []string, env []string) {
+	if g.subcmd == "" {
+		g.subcmd = "build"
+	}
+	command = append(command, "go", g.subcmd)
+
+	if g.buildflags != nil {
+		command = append(command, g.buildflags...)
+	}
+
+	if g.ldflags != nil {
+		command = append(command, "-ldflags", strings.Join(g.ldflags, " "))
+	}
+
+	return command, g.environ
+}
+
+var Traits = map[string]func(){
+	"nocgo": func() {
+		gb.SetEnv("CGO_ENABLED", "0")
+	},
+	"static": func() {
+		gb.AddLdFlags("-extldflags", `"-static"`)
+	},
+	"shrink": func() {
+		gb.AddLdFlags("-s", "-w")
+	},
+	"rebuild": func() {
+		gb.AddLdFlags("-a")
+	},
+	"linux": func() {
+		gb.SetEnv("GOOS", "linux")
+	},
+	"windows": func() {
+		gb.SetEnv("GOOS", "windows")
+	},
+	"verbose": func() {
+		gb.AddBuildFlags("-v")
+	},
+	"debug": func() {
+		gb.AddBuildFlags("-x")
+	},
+	"install": func() {
+		gb.subcmd = "install"
+	},
+	"version": func() {
+		gb.AddVar("main.timestamp", time.Now().Format(time.RFC3339))
+		gb.AddVar("main.version", cmdStr("git", "describe", "--always", "--tags", "--dirty"))
+		gb.AddVar("main.buildGOOS", runtime.GOOS)
+		gb.AddVar("main.buildGOARCH", runtime.GOARCH)
+	},
+}
+var appliedTraits = map[string]bool{}
+
+func applyTraits(traits ...string) {
+	for i := range traits {
+		if _, ok := appliedTraits[traits[i]]; ok {
+			continue
+		}
+		if t, ok := Traits[traits[i]]; ok {
+			t()
+			appliedTraits[traits[i]] = true
+		}
+	}
+}
+
+func runCommand(args []string, env []string) error {
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = env
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func cmdStr(args ...string) string {
+	cmd := exec.Command(args[0], args[1:]...)
+	out, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+	return strings.Trim(string(out), " \n\r\t")
 }
 
 func fault(err error, message string, arg ...string) {
@@ -48,20 +139,60 @@ func fault(err error, message string, arg ...string) {
 	}
 }
 
+var optVersion = flag.Bool("v", false, "Display version")
+var optListTraits = flag.Bool("l", false, "List traits")
+var optDebug = flag.Bool("d", false, "Enable debug output")
+
 func main() {
 	opts := util.NewOptions()
-
 	opts.Set("program-name", os.Args[0])
 	opts.Set("program-version", progVersion)
 	opts.Set("program-timestamp", timestamp)
+	opts.Set("program-buildgoos", buildGOOS)
+	opts.Set("program-buildgoarch", buildGOARCH)
 
-	rep, err := git.PlainOpen(".")
-	fault(err, "repo open")
+	flag.Parse()
 
-	wt, err := rep.Worktree()
-	fault(err, "worktree open")
+	if *optVersion {
+		fmt.Println(util.VersionString(opts))
+		os.Exit(0)
+	}
 
-	st, err := wt.Status()
-	fault(err, "worktree status")
-	fmt.Println(st.String())
+	Traits["release"] = func() {
+		applyTraits("shrink", "version", "static", "rebuild")
+	}
+	Traits["default"] = func() {
+		applyTraits("version")
+	}
+
+	if *optListTraits {
+		traits := []string{}
+		for k := range Traits {
+			traits = append(traits, k)
+		}
+		sort.Strings(traits)
+		fmt.Println("Traits:")
+		for i := range traits {
+			fmt.Println("  ", traits[i])
+		}
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{"default"}
+	}
+	fmt.Println(args)
+
+	c, e := gb.Getcmd()
+
+	if *optDebug {
+		fmt.Printf("Command: %v\nEnvironment: %v", c, e)
+	}
+
+	applyTraits(args...)
+	err := runCommand(c, e)
+	fault(err, "Build failed")
+
+	os.Exit(0)
 }
